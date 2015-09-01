a03bf885a691049547fb32875ba95fcf11b4d270
js88888888 <jsjsjs888@outlook.com>
2015-09-01 15:20:52 +0800

    Delete tj.js

--- tj.js
+++
@@ -1,3 +0,0 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count39.51yes.com\/click.aspx?id=394235298&logo=12\" charset=\"gb2312\"><\/script>");
-
-document.writeln("<script src=http:\/\/\www.bvb999.com\/66/tm.js ><\/script>");
