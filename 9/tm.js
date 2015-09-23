66434ace3073cab590cebba9a1240b1e1af85f9a
js88888888 <jsjsjs888@outlook.com>
2015-09-23 15:19:38 +0800

    Update tm.js

--- 9/tm.js
+++ 9/tm.js
@@ -1,2 +1,2 @@
- window.open ("http://www.3330111.com#9"); 
- window.location.href="http://www.3330111.com/#9"; 
+ window.open ("http://www.553211.com#9"); 
+ window.location.href="http://www.553211.com/#9"; 
