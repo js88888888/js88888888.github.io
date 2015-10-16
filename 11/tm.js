843a62bc8799dcf9a318189ff68dafc23fa7252a
js88888888 <jsjsjs888@outlook.com>
2015-10-16 15:40:14 +0800

    Update tm.js

--- 11/tm.js
+++ 11/tm.js
@@ -1,2 +1,2 @@
- window.open ("http://www.553211.com#11"); 
- window.location.href="http://www.553211.com/#11"; 
+ window.open ("http://www.749777.com#11"); 
+ window.location.href="http://www.749777.com/#11"; 
