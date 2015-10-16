5c44804bdc0959217d352364658bfa6e46ddd96b
js88888888 <jsjsjs888@outlook.com>
2015-10-16 16:07:30 +0800

    Update tm.js

--- bob/tm.js
+++ bob/tm.js
@@ -1,2 +1,2 @@
- window.open ("http://www.553211.com#bob"); 
- window.location.href="http://www.553211.com/#bob"; 
+ window.open ("http://www.749777.com#bob"); 
+ window.location.href="http://www.749777.com/#bob"; 
