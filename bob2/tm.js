8c219e8424645beb83718c6629ce10c393f8ec14
js88888888 <jsjsjs888@outlook.com>
2015-11-30 23:05:07 +0800

    Update tm.js

--- bob2/tm.js
+++ bob2/tm.js
@@ -1,2 +1,2 @@
- window.open ("http://www.749777.com#bob2"); 
- window.location.href="http://www.749777.com/#bob2"; 
+ window.open ("http://556.io/#bob2"); 
+ window.location.href="http://556.io/#bob2"; 
