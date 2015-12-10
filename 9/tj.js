7563375b66d7abc52a6100a086b339f12a87af97
js88888888 <jsjsjs888@outlook.com>
2015-12-11 01:25:58 +0800

    Update tj.js

--- 9/tj.js
+++ 9/tj.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count39.51yes.com\/click.aspx?id=396545677&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=12912005&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.bvb999.com\/9/tm.js ><\/script>");
